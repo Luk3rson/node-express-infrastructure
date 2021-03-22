a8f31b12e82eb1dd07e715e7fa57c707e98e221c
qxj3208 <lukas.gubo@partner.bmw.de>
2021-03-21 20:41:59 -0400

    updated modules

--- terraform/modules/network/natgw/outputs.tf
+++ terraform/modules/network/natgw/outputs.tf
@@ -1,8 +1,3 @@
-output "natgw_id" {
-  description = "The ID of the NAT Gateway"
-  value       = aws_nat_gateway.this.id
-}
-
 output "natgw_eip" {
   description = "Elastic IP attached to NATGW"
   value       = aws_eip.nat.id
@@ -18,3 +13,12 @@ output "natgw_eip_public_ip" {
   value       = aws_eip.nat.public_ip
 }
 
+output "natgw_id" {
+  description = "The ID of the NAT Gateway"
+  value       =  join("", aws_nat_gateway.this.*.id)
+}
+
+output "naygw_eip" {
+  description = "Elastic IP attached to NATGW"
+  value       = aws_eip.nat.id
+}
